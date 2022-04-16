28dabfbf55f7fbfcd69fb1d078b2eca6c7521d65
權少 <ljqican@163.com>
2022-04-16 14:41:40 +0800

    首次提交。

---
+++ include/coro.hpp
@@ -0,0 +1,129 @@
+#pragma once
+#include <memory>
+#include <string>
+#include <vector>
+#include <tuple>
+#define BOOST_ASIO_NO_DEPRECATED 1
+#include <boost/asio.hpp>
+#include <boost/system.hpp>
+#include <coroutine> // 引入協程
+
+#ifndef SPDLOG_EXISTS
+#  define SPDLOG_EXISTS 0
+#endif
+
+#if SPDLOG_EXISTS
+#  include <spdlog/spdlog.h>
+#else
+#  include <iostream>
+#  define SPDLOG_INFO(x) std::cout << (x) << "\n";
+#endif
+
+namespace asio = boost::asio;
+using error_code_t = boost::system::error_code;
+
+namespace lite
+{
+    struct task
+    {
+        struct promise_type
+        {
+            task get_return_object() { return {}; }
+            std::suspend_never initial_suspend() { return {}; }
+            std::suspend_never final_suspend() noexcept { return {}; }
+            void return_void() {}
+            void unhandled_exception()
+            {
+                std::exit(1);
+            }
+        };
+    };
+
+    template<typename F>
+    struct awaiter
+    {
+        awaiter(F _f) : f(_f) {}
+        bool await_ready() { return false; }
+        void await_suspend(std::coroutine_handle<> _handle) // _handle為傳入的協程句柄
+        {
+            f(_handle);
+        }
+        void await_resume() {}
+        F f;
+    };
+
+    template<typename F>
+    inline auto await(std::coroutine_handle<>& _handle, F _f)
+    {
+        return awaiter{
+            [&_handle, _f](std::coroutine_handle<> _hdl)
+            {
+                _handle = _hdl; // 把協程句柄傳出去
+                _f();
+            }
+        };
+    }
+
+    template<typename T, typename F>
+    struct awaiter_value
+    {
+        awaiter_value(F _f) : f(_f) {}
+        bool await_ready() { return false; }
+        void await_suspend(std::coroutine_handle<> _handle)
+        {
+            f(_handle, value);
+        }
+        T await_resume() { return value; }
+        F f;
+        T value{};
+    };
+
+    template<typename T, typename F>
+    inline auto await_value(std::coroutine_handle<>& _handle, F _f)
+    {
+        auto fn = [&_handle, _f](std::coroutine_handle<> _hdl, T& _value)
+        {
+            _handle = _hdl;
+            _f(_value);
+        };
+        return awaiter_value<T, decltype(fn)>{ fn };
+    }
+
+    inline auto async_read(
+        std::shared_ptr<std::coroutine_handle<>> _handle,
+        asio::ip::tcp::socket& _socket,
+        asio::mutable_buffer _buffer)
+    {
+        using result_t = std::tuple<error_code_t, std::size_t>;
+        return lite::await_value<result_t>(
+            *_handle,
+            [_handle, &_socket, _buffer](result_t& _result)
+            {
+                _socket.async_read_some(_buffer,
+                    [_handle, &_result](error_code_t _error, std::size_t _bytes)
+                    {
+                        _result = std::make_tuple(_error, _bytes);
+                        _handle->resume();
+                    });
+            });
+    }
+
+    inline auto async_write(
+        std::shared_ptr<std::coroutine_handle<>> _handle,
+        asio::ip::tcp::socket& _socket,
+        asio::const_buffer _buffer)
+    {
+        using result_t = std::tuple<error_code_t, std::size_t>;
+        return lite::await_value<result_t>(
+            *_handle,
+            [_handle, &_socket, _buffer](result_t& _result)
+            {
+                _socket.async_write_some(_buffer,
+                    [_handle, &_result](error_code_t _error, std::size_t _bytes)
+                    {
+                        _result = std::make_tuple(_error, _bytes);
+                        _handle->resume();
+                    });
+            });
+    }
+}
---
+++ src/server.cpp
@@ -0,0 +1,165 @@
+#include <coro.hpp>
+
+namespace asio = boost::asio;
+using error_code_t = boost::system::error_code;
+
+void server_callback(std::shared_ptr<asio::ip::tcp::socket> _socket)
+{
+    auto vec = std::make_shared<std::vector<char>>(1024, '\0');
+    _socket->async_read_some(
+        asio::buffer(vec->data(), vec->size()),
+        [_socket, vec](error_code_t _error, std::size_t _bytes)
+        {
+            if (_error)
+            {
+                SPDLOG_INFO(_error.message());
+                return;
+            }
+            SPDLOG_INFO(_bytes);
+
+            SPDLOG_INFO(vec->data());
+
+            auto str = std::make_shared<std::string>("server.");
+            _socket->async_write_some(
+                asio::buffer(str->c_str(), str->size()),
+                [_socket, str](error_code_t _error, std::size_t _bytes)
+                {
+                    if (_error)
+                    {
+                        SPDLOG_INFO(_error.message());
+                        return;
+                    }
+                    SPDLOG_INFO(_bytes);
+                });
+        });
+}
+
+lite::task server_coro(std::shared_ptr<asio::ip::tcp::socket> _socket)
+{
+    auto& socket = *_socket;
+    auto handle = std::make_shared<std::coroutine_handle<>>();
+
+    error_code_t error;
+    std::size_t bytes;
+
+    std::vector<char> vec(1024, '\0');
+    co_await lite::await(*handle,
+        [handle, &socket, &error, &bytes, &vec]()
+        {
+            socket.async_read_some(asio::buffer(vec.data(), vec.size()),
+                [handle, &error, &bytes](error_code_t _error, std::size_t _bytes)
+                {
+                    error = _error;
+                    bytes = _bytes;
+                    handle->resume();
+                });
+        });
+    if (error)
+    {
+        SPDLOG_INFO(error.message());
+        co_return;
+    }
+    SPDLOG_INFO(bytes);
+    SPDLOG_INFO(vec.data());
+
+    std::string str{ "server." };
+    co_await lite::await(*handle,
+        [handle, &socket, &error, &bytes, &str]()
+        {
+            socket.async_write_some(asio::buffer(str.c_str(), str.size()),
+                [handle, &error, &bytes](error_code_t _error, std::size_t _bytes)
+                {
+                    error = _error;
+                    bytes = _bytes;
+                    handle->resume();
+                });
+        });
+    if (error)
+    {
+        SPDLOG_INFO(error.message());
+        co_return;
+    }
+    SPDLOG_INFO(bytes);
+    SPDLOG_INFO("write finished.");
+}
+
+lite::task server_coro_value(std::shared_ptr<asio::ip::tcp::socket> _socket)
+{
+    auto& socket = *_socket;
+    auto handle = std::make_shared<std::coroutine_handle<>>(); // 用於保存協程句柄
+
+    std::vector<char> vec(1024, '\0');
+    auto [error_r, bytes_r] = co_await lite::async_read(handle, socket, asio::buffer(vec.data(), vec.size()));
+    if (error_r)
+    {
+        SPDLOG_INFO(error_r.message());
+        co_return;
+    }
+    SPDLOG_INFO(bytes_r);
+    SPDLOG_INFO(vec.data());
+
+    std::string str{ "server." };
+    auto [error_w, bytes_w] = co_await lite::async_write(handle, socket, asio::buffer(str.c_str(), str.size()));
+    if (error_w)
+    {
+        SPDLOG_INFO(error_w.message());
+        co_return;
+    }
+    SPDLOG_INFO(bytes_w);
+    SPDLOG_INFO("write finished.");
+}
+
+void on_accept(
+    error_code_t _error,
+    asio::ip::tcp::acceptor& _acceptor,
+    std::shared_ptr<asio::ip::tcp::socket> _socket)
+{
+    if (_error)
+    {
+        SPDLOG_INFO(_error.message());
+        return;
+    }
+
+    auto newSocket = std::make_shared<asio::ip::tcp::socket>(_acceptor.get_executor());
+    _acceptor.async_accept(
+        *newSocket,
+        [&_acceptor, newSocket](error_code_t error)
+        {
+            on_accept(error, _acceptor, newSocket);
+        });
+
+    auto remote_endpoint = _socket->remote_endpoint();
+    SPDLOG_INFO(remote_endpoint.address().to_string());
+    SPDLOG_INFO(remote_endpoint.port());
+
+    //server_callback(_socket); // 回調版本
+    //server_coro(_socket); // 協程包裹回調
+    server_coro_value(_socket); // 協程封裝回調
+}
+
+int main()
+{
+#if SPDLOG_EXISTS
+    spdlog::set_pattern("[%Y-%m-%d %T.%e] [%^%l%$] [t:%6t] [p:%6P] [%-20!!:%4#] %v");
+#endif
+
+    asio::io_context io_context;
+
+    asio::ip::tcp::acceptor acceptor(
+        io_context,
+        asio::ip::tcp::endpoint(asio::ip::tcp::v4(), 8888));
+
+    {
+        auto socket = std::make_shared<asio::ip::tcp::socket>(io_context);
+        acceptor.async_accept(
+            *socket,
+            [&acceptor, socket](error_code_t error)
+            {
+                on_accept(error, acceptor, socket);
+            });
+    }
+
+    io_context.run();
+
+    return 0;
+}
